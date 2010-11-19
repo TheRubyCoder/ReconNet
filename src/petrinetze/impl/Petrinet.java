ff738682b86062bd60d2e0e7cc1f59e06f9bdf7f
Zabihullah Safai <zabihullah.safai@binaroo.com>
2010-11-19 09:12:20 +0000

    ActionListener und weitere Methoden hinzugefuegt
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@43 3f72c222-a292-0410-9194-aba524b78705

--- src/petrinetze/impl/Petrinet.java
+++ src/petrinetze/impl/Petrinet.java
@@ -1,17 +1,25 @@
 package petrinetze.impl;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import petrinetze.ActionType;
 import petrinetze.IArc;
+import petrinetze.IGraphElement;
 import petrinetze.INode;
 import petrinetze.IPetrinet;
 import petrinetze.IPlace;
 import petrinetze.IPost;
 import petrinetze.IPre;
 import petrinetze.ITransition;
+import petrinetze.PetrinetListener;
 
 public class Petrinet implements IPetrinet {
-
+	
+	private final Set<PetrinetListener> listeners = new HashSet<PetrinetListener>();
+ 
 	@Override
 	public IPlace createPlace(String name) {
 		return new Place();
@@ -34,7 +42,9 @@ public class Petrinet implements IPetrinet {
 
 	@Override
 	public IArc createArc() {
-		return null;
+		final IArc arc = null;
+		fireChanged(arc);
+		return arc;
 	}
 
 	@Override
@@ -43,17 +53,17 @@ public class Petrinet implements IPetrinet {
 	}
 
 	@Override
-	public List<ITransition> getActivatedTransitions() {
+	public Set<ITransition> getActivatedTransitions() {
 		return null;
 	}
 
 	@Override
-	public List<INode> fire(int id) {
+	public Set<INode> fire(int id) {
 		return null;
 	}
 
 	@Override
-	public List<INode> fire() {
+	public Set<INode> fire() {
 		return null;
 	}
 
@@ -72,4 +82,51 @@ public class Petrinet implements IPetrinet {
 		return 0;
 	}
 
+	private void fireChanged(INode element) {
+		final List<PetrinetListener> listeners;
+		
+		synchronized (this.listeners){
+			listeners = new ArrayList<PetrinetListener>(this.listeners);
+		}
+		
+		for (PetrinetListener l : listeners)  {
+			l.changed(this, element, ActionType.changed);
+		}
+	}
+
+	@Override
+	public Set<IPlace> getAllPlaces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<ITransition> getAllTransitions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<IArc> getAllArcs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<IGraphElement> getAllGraphicElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPetrinetListener(PetrinetListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePetrinetListener(PetrinetListener l) {
+		// TODO Auto-generated method stub
+		
+	}
 }
