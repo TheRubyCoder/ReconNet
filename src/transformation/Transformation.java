1dc2aa7f6d56af58798b42dbf4b6de1350a48bb9
aav673 <undisclosed@example.com>
2010-11-19 09:13:14 +0000

    Constructor, Local Var. etc. added
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@45 3f72c222-a292-0410-9194-aba524b78705

--- src/transformation/Transformation.java
+++ src/transformation/Transformation.java
@@ -3,28 +3,53 @@ package transformation;
 import petrinetze.IPetrinet;
 
 public class Transformation implements ITransformation {
-
+	
+	private final IPetrinet N;
+	private final IMorphism morphism;
+	private final IRule rule;
+	
+	/**
+	 * Constructor for the class Transformation
+	 */	
+	public Transformation(IPetrinet n,IMorphism morph,IRule r){
+		N = n; morphism = morph; rule = r;
+		
+	}		
+	/**
+	 * Returns the IPetrinet of this transformation.
+	 * This net will be changed when transform() is called.
+	 * @return the IRule of this transformation.
+	 */
 	@Override
 	public IPetrinet N() {
-		// TODO Auto-generated method stub
-		return null;
+		return N;
 	}
-
+	
+	/**
+	 * Returns the IMorphism of this transformation.
+	 * @return the IMorphism of this transformation.
+	 */
 	@Override
 	public IMorphism morphism() {
 		// TODO Auto-generated method stub
-		return null;
+		return morphism;
 	}
-
+	
+	/**
+	 * Returns the IRule of this transformation.
+	 * @return the IRule of this transformation.
+	 */	
 	@Override
 	public IRule rule() {
 		// TODO Auto-generated method stub
-		return null;
+		return rule;
 	}
-
+	/**
+	 * Method for starting the transform. 
+	 */
 	@Override
 	public void transform() {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub, Put code in ! 
 
 	}
 
