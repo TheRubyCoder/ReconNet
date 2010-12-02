a854ba58f2eee67ab9dfecf6b6d0a996ac899b6d
Zabihullah Safai <zabihullah.safai@binaroo.com>
2010-12-02 13:10:22 +0000

    Methoden getPre und getPost definiert
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@75 3f72c222-a292-0410-9194-aba524b78705

--- src/petrinetze/IPre.java
+++ src/petrinetze/IPre.java
@@ -7,5 +7,20 @@ package petrinetze;
  *
  */
 public interface IPre {
-
+	/**
+	 * Liefert das Pre von dem entsprechenden Petrinetz
+	 * Die Identifier der Stellen und Transition werden 
+	 * jeweils als nulltes Element des Arrays abgebildet
+	 * Bsp.
+	 * 
+	 * 	  1 3 6 
+	 *  1 1 3 2
+	 *  2 2 5 3	
+	 *  3 1 3 1
+	 *  
+	 * @return
+	 * Nullte Stelle senkrecht: Id von Stellen
+	 * Nullte Stelle waagerecht: Id von Transitionen 
+	 */
+	public int [][] getPreAsArray ();
 }
