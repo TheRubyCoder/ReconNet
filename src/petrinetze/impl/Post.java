536774d9c53faef468c83b5195e5d006092088bf
Zabihullah Safai <zabihullah.safai@binaroo.com>
2010-12-23 22:23:53 +0000

    Pre und Post eingebaut
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@157 3f72c222-a292-0410-9194-aba524b78705

--- src/petrinetze/impl/Post.java
+++ src/petrinetze/impl/Post.java
@@ -1,25 +1,56 @@
 package petrinetze.impl;
 
+import java.util.Arrays;
+
 import petrinetze.IPost;
 
 public class Post implements IPost {
 
+	private int [][] post;
+	private int []tIds;
+	private int []pIds;
+	
+	public Post (int[][] post, int[] pId, int[] tId) {
+		this.pIds = pId;
+		this.tIds = tId;
+		this.post = post;
+		
+		
+	}
+	
 	@Override
-	public int[][] getPostAsArray() {
-		// TODO Auto-generated method stub
-		return null;
+	public int[][] getPreAsArray() {
+		return this.post;
 	}
 
 	@Override
 	public int[] getTransitionIds() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.tIds;
 	}
 
 	@Override
 	public int[] getPlaceIds() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.pIds;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String output = "\n";
+		
+			for (int i = 0; i < pIds.length; i++) {
+				for (int z = 0; z < tIds.length; z++) {
+					output += " [" + post[i][z] + "]";
+				}
+				output += "\n";
+			}
+			
+
+		return "Post [post=" + output + ", tIds="
+				+ Arrays.toString(tIds) + ", pIds=" + Arrays.toString(pIds)
+				+ "]";
 	}
 
 }
