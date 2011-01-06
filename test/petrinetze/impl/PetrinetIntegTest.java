636921af9d6d195f5a4f1398e3213798388f5bcf
Zabihullah Safai <zabihullah.safai@binaroo.com>
2011-01-06 21:11:54 +0000

    Kleine Anpassungen
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@167 3f72c222-a292-0410-9194-aba524b78705

--- test/petrinetze/impl/PetrinetIntegTest.java
+++ test/petrinetze/impl/PetrinetIntegTest.java
@@ -15,8 +15,12 @@ public class PetrinetIntegTest {
 	public static void main(String[] args) {
 		IPetrinet net = new Petrinet();
 		IPlace p1 = net.createPlace("AAA");
+		p1.setMark(3);
 		IPlace p2 = net.createPlace("BBB");
 		IPlace p3 = net.createPlace("CCC");
+		IPlace p4 = net.createPlace("DDD");
+		
+		p2.setMark(6);
 		
 		ITransition t1 = net.createTransition("ttt");
 		ITransition t2 = net.createTransition("uuu");
@@ -26,9 +30,16 @@ public class PetrinetIntegTest {
 		IArc a1 = net.createArc("at", p1, t1);
 		a1.setMark(2);
 		
+		IArc a5 = net.createArc("at", t2, p4);
+		a1.setMark(2);
+		
 		IArc a2 = net.createArc("bt", t1, p2);
 		IArc a3 = net.createArc("ct", t1, p3);
 		a3.setMark(3);
+		
+		for (IArc a : net.getAllArcs()) {
+			System.out.println("Arc ID: " + a.getId() + " Name: " + a.getName());
+		}
 		
 		IPre pre = net.getPre();
 		System.out.println(pre);
@@ -36,6 +47,21 @@ public class PetrinetIntegTest {
 		IPost post = net.getPost();
 		System.out.println(post);
 		
+//		net.deleteTransitionByID(2);
+//		net.deletePlaceById(2);
+		
+		IPre pre1 = net.getPre();
+		System.out.println(pre1);
+		
+		net.fire();
+		
+		IPost post1 = net.getPost();
+		System.out.println(post1);
+		for (IArc a : net.getAllArcs()) {
+			System.out.println("Arc ID: " + a.getId() + " Name: " + a.getName());
+		}
+		
+		System.out.println(net.getActivatedTransitions());
 	}
 
 }
