7874dfd91a871ae15f502c99d540e98f41600e29
Maximilian Meyer <undisclosed@example.com>
2011-11-08 13:57:40 +0000

    Comments extended/translated to english
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@281 3f72c222-a292-0410-9194-aba524b78705

--- src/engine/impl/SimulationImpl.java
+++ src/engine/impl/SimulationImpl.java
@@ -42,18 +42,32 @@ class SimulationImpl implements Simulation {
     SimulationImpl(EngineContext context) {
         this.context = context;
     }
-
+    
+    /**
+     * lets a transition do a step and fires an SteppedEvent to all Listeners
+     */
+    
     @Override
     public void step() {
         context.getPetrinet().fire();
         fireStepped();
     }
 
+    /**
+     * lets a choosen transition do a step and fires an SteppedEvent to all Listeners
+     * 
+     * @param transition transition where the step should be done
+     */   
     public void step(ITransition transition) {
         context.getPetrinet().fire(transition.getId());
         fireStepped();
     }
 
+    /**
+     * starts the continuous simulation and fires a StartedEvent 
+     * 
+     * @param delay time to wait after each step
+     */
     @Override
     public void start(long delay) {
         if (logger.isLoggable(Level.FINE)) {
@@ -64,6 +78,9 @@ class SimulationImpl implements Simulation {
         fireStarted();
     }
 
+    /**
+     * stops the continuous simulation and fires a StoppedEvent
+     */
     @Override
     public void stop() {
         if (logger.isLoggable(Level.FINE)) {
@@ -72,7 +89,12 @@ class SimulationImpl implements Simulation {
         timer.stop();
         fireStopped();
     }
-
+    
+    /**
+     * adds a stepListener to the list of listeners
+     * 
+     * @param listener you want to add
+     */
     @Override
     public void addStepListener(StepListener listener) {
         synchronized (listeners) {
@@ -81,22 +103,32 @@ class SimulationImpl implements Simulation {
             }
         }
     }
-
+    
+    /**
+     * removes a stepListener from the list of listeners
+     * 
+     * @param listener you want to remove
+     */
     @Override
     public void removeStepListener(StepListener listener) {
         synchronized (listeners) {
             listeners.remove(listener);
         }
     }
-
+    
+    /**
+     * checks if simulation is running
+     * 
+     * @return <code> true </code> if simulation is running, <code> false </code> otherwise
+     */
     public boolean isRunning() {
         return timer.isRunning();
     }
 
     /**
-     * Synchronisierter Zugriff auf die {@link StepListener}.
+     * synchronized acces to the List of {@link StepListener}.
      *
-     * @return Kopie der Listener-Liste
+     * @return copy of the list of listeners
      */
     private List<StepListener> allListeners() {
         synchronized (listeners) {
@@ -105,7 +137,7 @@ class SimulationImpl implements Simulation {
     }
 
     /**
-     * Feuern eines Events beim Start der Simulation.
+     * fires an event when starting the simulation
      */
     protected void fireStarted() {
         for (StepListener l : allListeners()) {
@@ -119,7 +151,7 @@ class SimulationImpl implements Simulation {
     }
 
     /**
-     * Feuern eines Events beim Stoppen der Simulation.
+     * fires an event when stopping the simulation
      */
     protected void fireStopped() {
         for (StepListener l : allListeners()) {
@@ -133,7 +165,7 @@ class SimulationImpl implements Simulation {
     }
 
     /**
-     * Feuern eines Events bei Simulationsschritt.
+     * fires an event at a single step
      */
     protected void fireStepped() {
         for (StepListener l : allListeners()) {
