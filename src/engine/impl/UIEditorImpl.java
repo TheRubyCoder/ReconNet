88d66faaa8616d499323969ba98f565151d0cf98
aas772 <undisclosed@example.com>
2011-11-08 18:26:32 +0000

    comments
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@285 3f72c222-a292-0410-9194-aba524b78705

--- src/engine/impl/UIEditorImpl.java
+++ src/engine/impl/UIEditorImpl.java
@@ -1,21 +1,19 @@
 package engine.impl;
 
-import engine.UIEditor;
-import petrinetze.INode;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+
 import petrinetze.IPlace;
 import petrinetze.IRenew;
 import petrinetze.Renews;
-
-import java.awt.*;
-import java.util.*;
-import java.util.List;
+import engine.UIEditor;
 
 /**
- * Created by IntelliJ IDEA.
- * User: moritz
- * Date: 13.01.11
- * Time: 10:46
- * To change this template use File | Settings | File Templates.
+ * Klasse zum verwalten von RENEWS
  */
 public class UIEditorImpl implements UIEditor {
 
@@ -23,11 +21,19 @@ public class UIEditorImpl implements UIEditor {
 
     private final Map<IRenew,String> renews = new IdentityHashMap<IRenew,String>();
 
+    /**
+     * Konstruktor fuer diese Klasse.
+     */
     public UIEditorImpl() {
         renews.put(Renews.IDENTITY, "identity");
         renews.put(Renews.COUNT, "count");
     }
 
+    /**
+     * getPlacePaint gibt die Farbe von einem Vertex zurueck
+     * @param node ist der gewaehlte Knoten
+     * @return Color ist die zuruck zu gebende Farbe
+     */
     @Override
     public Color getPlacePaint(IPlace node) {
 
@@ -36,11 +42,21 @@ public class UIEditorImpl implements UIEditor {
         return p != null ? p : new Color(Color.HSBtoRGB((node.getId() / 23f), 0.5f + (node.getId() / 127f) * 0.25f, 1f));
     }
 
+    /**
+     * setPlacePaint faerbt einen Knoten in der angegebenen Farbe
+     * @param node der zu faerbende Vertex
+     * @param paint die zu waehlende Farbe
+     */
     @Override
     public void setPlacePaint(IPlace node, Color paint) {
         vertexPaint.put(node.getId(), paint);
     }
 
+    /**
+     * getColorMap gibt alle verwendeten Farben wieder
+     * @return Map<Integer, String> bestehend aus einem Integer und
+     * einer Farbe als String
+     */
     @Override
     public Map<Integer, String> getColorMap() {
         final Map<Integer,String> colorMap = new HashMap<Integer,String>(vertexPaint.size());
@@ -52,16 +68,35 @@ public class UIEditorImpl implements UIEditor {
         return colorMap;
     }
 
+    /**
+     * Gibt die Renew definition zurueck.
+     * @return List<IRenew> ist eine Liste mit allen
+     * Renews zurueck
+     */
     @Override
     public List<IRenew> getRenews() {
         return new ArrayList<IRenew>(renews.keySet());
     }
 
+    /**
+     * displayName gibt den zugehoerigen Namen
+     * zum renew zurueck.
+     * @param renew zu dem der Name gefunden werden soll
+     * @return String der zugehoerige String
+     */
     @Override
     public String displayName(IRenew renew) {
         return renews.get(renew);
     }
 
+    /**
+     * defineRenew wird benutzt um einen Renew zu erzeugen
+     * und dann in dem Set zu speichern.
+     * @param name Name des Renews
+     * @param f stellt als Map<String, String> ein Tupel von zwei Strings
+     * dar
+     * @return das erstellte Renew
+     */
     @Override
     public IRenew defineRenew(String name, Map<String,String> f) {
         IRenew rnw = Renews.fromMap(f);
@@ -69,6 +104,11 @@ public class UIEditorImpl implements UIEditor {
         return rnw;
     }
 
+    /**
+     * removeRenew entfernt einen Renew aus dem internen Set
+     * @param renew den zu entfernenden Renew
+     * @return true wenn es erfolgreich war; sonst false
+     */
     @Override
     public boolean removeRenew(IRenew renew) {
         if (renew == Renews.IDENTITY || renew == Renews.COUNT) {
