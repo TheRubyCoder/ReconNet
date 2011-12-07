0116f3b3f85929ea14dfe0fae32f2856ce96cc2d
aas772 <undisclosed@example.com>
2011-12-07 10:15:21 +0000

    IPersistance doc
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@355 3f72c222-a292-0410-9194-aba524b78705

--- src/persistence/IPersistance.java
+++ src/persistence/IPersistance.java
@@ -3,8 +3,29 @@ package persistence;
 import petrinet.INode;
 import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
 
+/**
+ * 
+ * Interface between Persistance and Engine.
+ * It includes two methods. A method to save and one to load. 
+ * 
+ * @author alex
+ *
+ */
 public interface IPersistance {
+	
+	/**
+	 * Save saves the Petrinet to a File.
+	 * @param pathAndFilename Path and Filename as String
+	 * @param petrinet save this object
+	 * @param layout of this Petrinet
+	 */
 	public void save(String pathAndFilename, petrinet.Petrinet petrinet, AbstractLayout<INode, Arc> layout);
 	
+	/**
+	 * Load an Petrinet.
+	 * @param pathAndFilename The File to load. Consists of path and filename.
+	 * @param handler A instance of the IPetrinetHandler
+	 */
 	public void load(String pathAndFilename, engine.ihandler.IPetrinetHandler handler);
+	
 }
