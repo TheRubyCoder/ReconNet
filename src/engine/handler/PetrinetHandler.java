5129058c729e58f00d27b96a3608d116b1e190cb
aas772 <undisclosed@example.com>
2011-12-11 15:38:04 +0000

    petrinethandler (fast) fertig geschrieben
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@366 3f72c222-a292-0410-9194-aba524b78705

--- src/engine/handler/PetrinetHandler.java
+++ src/engine/handler/PetrinetHandler.java
@@ -2,8 +2,8 @@ package engine.handler;
 
 import java.awt.geom.Point2D;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import petrinet.Arc;
@@ -60,6 +60,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 			// ***************************
 
 			// call JungModification
+			// TODO: catch!
 			jungModification.createArc(jungData, arc, fromPlace, toTransition);
 		} else if (this.getNodeType(from).equals(NodeType.Transition)
 				&& this.getNodeType(to).equals(NodeType.Place)) {
@@ -77,6 +78,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 			// ***************************
 
 			// call JungModification
+			// TODO: catch!
 			jungModification.createArc(jungData, arc, fromTransition, toPlace);
 		} else {
 			// else => error!
@@ -91,7 +93,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 	@Override
 	public boolean createPlace(int id, Point2D coordinate) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -104,6 +106,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 		// ***************************
 
 		// call JungModificator
+		// TODO: catch!
 		jungModification.createPlace(jungData, newPlace, coordinate);
 
 		// TODO: with ChangedPetrinetResult true/false
@@ -127,7 +130,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 	@Override
 	public boolean createTransition(int id, Point2D coordinate) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -140,6 +143,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 		// ***************************
 
 		// call JungModificator
+		// TODO: catch!
 		jungModification.createTransition(jungData, newTransition, coordinate);
 
 		// TODO: with ChangedPetrinetResult true/false
@@ -148,152 +152,238 @@ public class PetrinetHandler implements IPetrinetManipulation {
 
 	@Override
 	public boolean deleteArc(int id, Arc arc) {
-		// TODO Auto-generated method stub
-		return false;
+
+		// get the Petrinet from the id and SessionManager
+		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
+		Petrinet petrinet = petrinetData.getPetrinet();
+		JungData jungData = petrinetData.getJungData();
+		
+		// TODO: ChangePetrinetResult
+		petrinet.deleteArcByID(arc.getId());
+		
+		Collection<Arc> collArc = new HashSet<Arc>();
+		collArc.add(arc);
+		
+		Collection<INode> collINodes = new HashSet<INode>();
+		
+		jungModification.delete(jungData, collArc, collINodes);
+		
+		return true;
 	}
 
 	@Override
 	public boolean deletePlace(int id, INode place) {
-		// TODO Auto-generated method stub
-		return false;
+		// TODO : write with ChangePetrinetResult
+		return true;
 	}
 
 	@Override
 	public boolean deleteTransition(int id, INode transition) {
-		// TODO Auto-generated method stub
-		return false;
+		// TODO : write with ChangePetrinetResult
+		return true;
 	}
 
 	@Override
 	public ArcAttribute getArcAttribute(int id, Arc arc) {
-		
-		// get the Petrinet from the ip and SessionManager
-//		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
-//		Petrinet petrinet = petrinetData.getPetrinet();
-//		JungData jungData = petrinetData.getJungData();
-		
+
+		// get the Petrinet from the id and SessionManager
+		// PetrinetData petrinetData = sessionManager.getPetrninetData(id);
+		// Petrinet petrinet = petrinetData.getPetrinet();
+		// JungData jungData = petrinetData.getJungData();
+
 		int weight = arc.getMark();
-		
+
 		ArcAttribute arcAttribute = new ArcAttribute(weight);
-		
+
 		return arcAttribute;
-		
+
 	}
 
 	@Override
 	public AbstractLayout<INode, Arc> getJungLayout(int id) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
-		
+
 		// TODO: Mathias =)
-		
+
 		return null;
 	}
 
 	@Override
 	public PlaceAttribute getPlaceAttribute(int id, INode place) {
 
-		// get the Petrinet from the ip and SessionManager
-//		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
-//		Petrinet petrinet = petrinetData.getPetrinet();
-//		JungData jungData = petrinetData.getJungData();
-		
-		if(this.getNodeType(place).equals(NodeType.Place)){
+		// get the Petrinet from the id and SessionManager
+		// PetrinetData petrinetData = sessionManager.getPetrninetData(id);
+		// Petrinet petrinet = petrinetData.getPetrinet();
+		// JungData jungData = petrinetData.getJungData();
+
+		if (this.getNodeType(place).equals(NodeType.Place)) {
 			Place p = (Place) place;
-			
+
 			int marking = p.getMark();
 			String pname = p.getName();
-			
+
 			PlaceAttribute placeAttribute = new PlaceAttribute(marking, pname);
-			
+
 			return placeAttribute;
 		}
-		
+
 		return null;
 	}
 
 	@Override
 	public TransitionAttribute getTransitionAttribute(int id, INode transition) {
 
-		// get the Petrinet from the ip and SessionManager
-//		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
-//		Petrinet petrinet = petrinetData.getPetrinet();
-//		JungData jungData = petrinetData.getJungData();
-		
-		if(this.getNodeType(transition).equals(NodeType.Transition)){
+		// get the Petrinet from the id and SessionManager
+		// PetrinetData petrinetData = sessionManager.getPetrninetData(id);
+		// Petrinet petrinet = petrinetData.getPetrinet();
+		// JungData jungData = petrinetData.getJungData();
+
+		if (this.getNodeType(transition).equals(NodeType.Transition)) {
 			Transition t = (Transition) transition;
-			
+
 			String tlb = t.getTlb();
 			String tname = t.getName();
 			IRenew rnw = t.getRnw();
-			
-			TransitionAttribute transitionAttribute = new TransitionAttribute(tlb, tname, rnw);
-			
+
+			TransitionAttribute transitionAttribute = new TransitionAttribute(
+					tlb, tname, rnw);
+
 			return transitionAttribute;
 		}
-		
+
 		return null;
 	}
 
 	@Override
 	public boolean moveGraph(int id, Point2D relativePosition) {
-		
-		// get the Petrinet from the ip and SessionManager
+
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
-		
+
 		// step 1 alles durchgehen kleinstes x,y ; groeßstes x,y
-		Map<INode, NodeLayoutAttribute> layoutMap = utility.getNodeLayoutAttributes(jungData);
-		
+		Map<INode, NodeLayoutAttribute> layoutMap = utility
+				.getNodeLayoutAttributes(jungData);
+
 		double smallestX = Double.MAX_VALUE;
 		double smallestY = Double.MAX_VALUE;
 		double biggestX = 0;
 		double biggestY = 0;
-		
+
 		Collection<NodeLayoutAttribute> entrySet = layoutMap.values();
-		
-		for(NodeLayoutAttribute n : entrySet){
+
+		for (NodeLayoutAttribute n : entrySet) {
 			double x = n.getCoordinate().getX();
 			double y = n.getCoordinate().getY();
-			
-			if(x < smallestX) smallestX = x;
-			if(x > biggestX) biggestX = x;
-			
-			if(y < smallestY) smallestY = y;
-			if(y > biggestY) biggestY = y;
+
+			if (x < smallestX)
+				smallestX = x;
+			if (x > biggestX)
+				biggestX = x;
+
+			if (y < smallestY)
+				smallestY = y;
+			if (y > biggestY)
+				biggestY = y;
 		}
-		
-		// wenn die box aus dem breich lauft wird sie an 0/0 angedockt
-		
-		// TODO: weiter machen!!!
-		
-		// step 2 alles durchgehen jungModifcation.moveNode(...)
-		
-		return false;
+
+		double smallestXMove = smallestX + relativePosition.getX();
+		double smallestYMove = smallestY + relativePosition.getY();
+
+		double addX = relativePosition.getX();
+		double addY = relativePosition.getY();
+
+		// Move Graph
+		if (smallestXMove < 0 || smallestYMove < 0) {
+			addX = relativePosition.getX() - smallestXMove;
+			addY = relativePosition.getY() - smallestYMove;
+		}
+
+		// For all Places
+		Set<Place> places = petrinet.getAllPlaces();
+		for (Place p : places) {
+			NodeLayoutAttribute nodeLayoutAttribute = layoutMap.get(p);
+
+			Point2D point = new Point2D.Double(
+					addX + nodeLayoutAttribute.getCoordinate().getX(), 
+					addY + nodeLayoutAttribute.getCoordinate().getY());
+
+			// TODO: catch!
+			jungModification.moveNode(jungData, p, point);
+		}
+
+		// For all Transitions
+		Set<Transition> transitions = petrinet.getAllTransitions();
+		for (Transition t : transitions) {
+			NodeLayoutAttribute nodeLayoutAttribute = layoutMap.get(t);
+
+			Point2D point = new Point2D.Double(
+					addX + nodeLayoutAttribute.getCoordinate().getX(), 
+					addY + nodeLayoutAttribute.getCoordinate().getY());
+
+			// TODO: catch!
+			jungModification.moveNode(jungData, t, point);
+		}
+
+		return true;
 	}
 
 	@Override
 	public boolean moveNode(int id, INode node, Point2D relativePosition) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		// get the Petrinet from the id and SessionManager
+		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
+		Petrinet petrinet = petrinetData.getPetrinet();
+		JungData jungData = petrinetData.getJungData();
+
+		// step 1 alles durchgehen kleinstes x,y ; groeßstes x,y
+		Map<INode, NodeLayoutAttribute> layoutMap = utility
+				.getNodeLayoutAttributes(jungData);
+		
+		NodeLayoutAttribute nla = layoutMap.get(node);
+		
+		double newPointX = nla.getCoordinate().getX() + relativePosition.getX();
+		double newPointY = nla.getCoordinate().getY() + relativePosition.getY();
+		
+		if(newPointX < 0 || newPointY < 0){
+			
+			double x = relativePosition.getX() - newPointX;
+			double y = relativePosition.getY() - newPointY;
+
+			Point2D point = new Point2D.Double(x, y);
+			
+			// TODO: catch!
+			jungModification.moveNode(jungData, node, point);
+			
+		} else {
+			
+			Point2D point = new Point2D.Double(newPointX, newPointY);
+			
+			// TODO: catch!
+			jungModification.moveNode(jungData, node, point);
+			
+		}
+		
+		return true;
 	}
 
 	@Override
 	public boolean save(int id, String path, String filename, String format) {
-		
+
 		// TODO: Alex
-		
+
 		return false;
 	}
 
 	@Override
 	public boolean setMarking(int id, INode place, int marking) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -306,6 +396,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 			p.setMark(marking);
 
 			// call JungModification
+			// TODO: catch!
 			jungModification.updatePlace(jungData, p);
 
 			return true;
@@ -318,7 +409,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 	@Override
 	public boolean setPname(int id, INode place, String pname) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -331,6 +422,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 			p.setName(pname);
 
 			// call JungModification
+			// TODO: catch!
 			jungModification.updatePlace(jungData, p);
 
 			return true;
@@ -343,7 +435,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 	@Override
 	public boolean setTlb(int id, INode transition, String tlb) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -356,6 +448,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 			t.setTlb(tlb);
 
 			// call JungModification
+			// TODO: catch!
 			jungModification.updateTransition(jungData, t);
 
 			return true;
@@ -368,7 +461,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 	@Override
 	public boolean setTname(int id, INode transition, String tname) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -381,6 +474,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 			t.setName(tname);
 
 			// call JungModification
+			// TODO: catch!
 			jungModification.updateTransition(jungData, t);
 
 			return true;
@@ -393,7 +487,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 	@Override
 	public boolean setWeight(int id, Arc arc, int weight) {
 
-		// get the Petrinet from the ip and SessionManager
+		// get the Petrinet from the id and SessionManager
 		PetrinetData petrinetData = sessionManager.getPetrninetData(id);
 		Petrinet petrinet = petrinetData.getPetrinet();
 		JungData jungData = petrinetData.getJungData();
@@ -402,6 +496,7 @@ public class PetrinetHandler implements IPetrinetManipulation {
 		arc.setMark(weight);
 
 		// call JungModification
+		// TODO: catch!
 		jungModification.updateArc(jungData, arc);
 
 		return true;
