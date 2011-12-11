dc937a6a516e81d88fc860e849d6dc6e9c076010
Mathias Blumreiter <undisclosed@example.com>
2011-12-11 09:08:50 +0000

    jung-Graph Löschtest vergrößert
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@365 3f72c222-a292-0410-9194-aba524b78705

--- test/engine/jungmodification/JungModificationTest.java
+++ test/engine/jungmodification/JungModificationTest.java
@@ -135,6 +135,20 @@ public class JungModificationTest {
 		assertTrue(Double.compare(pointPositive.getX(), jungData.getJungLayout().getX(transition1)) == 0);
 		assertTrue(Double.compare(pointPositive.getY(), jungData.getJungLayout().getY(transition1)) == 0);
 	}
+	
+
+	@Test
+	public void testDelete_emptyCollections() {		
+		JungData jung = buildJung(p);
+		
+		Set<INode> nodesBefore = new HashSet<INode>(jung.getJungGraph().getVertices());
+		Set<Arc> arcsBefore    = new HashSet<Arc>(jung.getJungGraph().getEdges());
+		
+		jungMod.delete(jung, new HashSet<Arc>(), new HashSet<INode>());
+
+		assertTrue(nodesBefore.equals(new HashSet<INode>(jung.getJungGraph().getVertices())));
+		assertTrue(arcsBefore.equals(new HashSet<Arc>(jung.getJungGraph().getEdges())));
+	}
 
 	@Test
 	public void testDelete() {
@@ -152,40 +166,112 @@ public class JungModificationTest {
 		Set<Arc> arcsToContain = new HashSet<Arc>();		
 		Set<Arc> arcsToDelete  = new HashSet<Arc>();
 
-		p = PetrinetComponent.getPetrinet().createPetrinet();
+		Petrinet p = PetrinetComponent.getPetrinet().createPetrinet();
 
 		Place placeA = p.createPlace("A");
+		Place placeB = p.createPlace("B");
+		Place placeY = p.createPlace("Y");
 		Place placeZ = p.createPlace("Z");
 
 		Transition transition1  = p.createTransition("t1", Renews.COUNT);
+		Transition transition2  = p.createTransition("t2", Renews.COUNT);
+		Transition transition9  = p.createTransition("t9", Renews.COUNT);
 		Transition transition10 = p.createTransition("t10", Renews.COUNT);
-		
+
 		placesToContain.add(placeA);
+		placesToContain.add(placeB);
+		placesToDelete.add(placeY);
 		placesToDelete.add(placeZ);
-		
+
 		transitionsToContain.add(transition1);
+		transitionsToContain.add(transition2);
+		transitionsToDelete.add(transition9);
 		transitionsToDelete.add(transition10);
 
+		// Kanten von allen Places zu allen Transitions
+		
 		Arc arcA1  = p.createArc("xA1", placeA, transition1);
+		Arc arcA2  = p.createArc("xA2", placeA, transition2);
+		Arc arcA9  = p.createArc("xA9", placeA, transition9);
 		Arc arcA10 = p.createArc("xA10", placeA, transition10);
 
+		Arc arcB1  = p.createArc("xB1", placeB, transition1);
+		Arc arcB2  = p.createArc("xB2", placeB, transition2);
+		Arc arcB9  = p.createArc("xB9", placeB, transition9);
+		Arc arcB10 = p.createArc("xB10", placeB, transition10);
+
+		Arc arcY1  = p.createArc("xY1", placeY, transition1);
+		Arc arcY2  = p.createArc("xY2", placeY, transition2);
+		Arc arcY9  = p.createArc("xY9", placeY, transition9);
+		Arc arcY10 = p.createArc("xY10", placeY, transition10);
+
 		Arc arcZ1  = p.createArc("xZ1", placeZ, transition1);
+		Arc arcZ2  = p.createArc("xZ2", placeZ, transition2);
+		Arc arcZ9  = p.createArc("xZ9", placeZ, transition9);
 		Arc arcZ10 = p.createArc("xZ10", placeZ, transition10);
 
-		Arc arc1A  = p.createArc("x1A", transition1, placeA);
-		Arc arc10A = p.createArc("x10A", transition10, placeA);
-
-		Arc arc1Z  = p.createArc("x1Z", transition1, placeZ);
-		Arc arc10Z = p.createArc("x10Z", transition10, placeZ);
-
 		arcsToContain.add(arcA1);
-		arcsToContain.add(arc1A);
-
+		arcsToContain.add(arcA2);
+		arcsToDelete.add(arcA9);
 		arcsToDelete.add(arcA10);
+
+		arcsToContain.add(arcB1);
+		arcsToContain.add(arcB2);
+		arcsToDelete.add(arcB9);
+		arcsToDelete.add(arcB10);
+
+		arcsToDelete.add(arcY1);
+		arcsToDelete.add(arcY2);
+		arcsToDelete.add(arcY9);
+		arcsToDelete.add(arcY10);
+
 		arcsToDelete.add(arcZ1);
+		arcsToDelete.add(arcZ2);
+		arcsToDelete.add(arcZ9);
 		arcsToDelete.add(arcZ10);
+		
+		// Kanten von allen Trasitions zu allen Places
+
+		Arc arc1A  = p.createArc("x1A", transition1, placeA);
+		Arc arc1B  = p.createArc("x1B", transition1, placeB);
+		Arc arc1Y  = p.createArc("x1Y", transition1, placeY);
+		Arc arc1Z  = p.createArc("x1Z", transition1, placeZ);
+
+		Arc arc2A  = p.createArc("x2A", transition2, placeA);
+		Arc arc2B  = p.createArc("x2B", transition2, placeB);
+		Arc arc2Y  = p.createArc("x2Y", transition2, placeY);
+		Arc arc2Z  = p.createArc("x2Z", transition2, placeZ);
+
+		Arc arc9A  = p.createArc("x9A", transition9, placeA);
+		Arc arc9B  = p.createArc("x9B", transition9, placeB);
+		Arc arc9Y  = p.createArc("x9Y", transition9, placeY);
+		Arc arc9Z  = p.createArc("x9Z", transition9, placeZ);
+
+		Arc arc10A  = p.createArc("x10A", transition10, placeA);
+		Arc arc10B  = p.createArc("x10B", transition10, placeB);
+		Arc arc10Y  = p.createArc("x10Y", transition10, placeY);
+		Arc arc10Z  = p.createArc("x10Z", transition10, placeZ);
+
+		// Kanten in zu löschende und zu behaltende aufteilen
+		
+		arcsToContain.add(arc1A);
+		arcsToContain.add(arc1B);
+		arcsToDelete.add(arc1Y);
+		arcsToDelete.add(arc1Z);		
+
+		arcsToContain.add(arc2A);
+		arcsToContain.add(arc2B);
+		arcsToDelete.add(arc2Y);
+		arcsToDelete.add(arc2Z);		
+
+		arcsToDelete.add(arc9A);
+		arcsToDelete.add(arc9B);
+		arcsToDelete.add(arc9Y);
+		arcsToDelete.add(arc9Z);		
+
 		arcsToDelete.add(arc10A);
-		arcsToDelete.add(arc1Z);
+		arcsToDelete.add(arc10B);
+		arcsToDelete.add(arc10Y);
 		arcsToDelete.add(arc10Z);
 		
 		JungData jung = buildJung(p);
