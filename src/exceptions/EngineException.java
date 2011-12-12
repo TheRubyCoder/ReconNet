ee8d92ad553def8f31bad8eca2f8a871cce2834c
Maximilian Meyer <undisclosed@example.com>
2011-12-12 08:35:22 +0000

    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@368 3f72c222-a292-0410-9194-aba524b78705

---
+++ src/exceptions/EngineException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class EngineException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public EngineException(String message){
+		super(message);
+	}
+
+}
