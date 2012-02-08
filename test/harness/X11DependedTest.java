5c21916ac5cc94659a27f40b9180e6d6ca4fc868
aas772 <undisclosed@example.com>
2012-02-08 14:24:45 +0000

    blap
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/wppetrinetze/wppetrinetze/trunk@539 3f72c222-a292-0410-9194-aba524b78705

--- test/harness/X11DependedTest.java
+++ test/harness/X11DependedTest.java
@@ -1,11 +1,9 @@
 package harness;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-    engine.impl.EngineTest.class
-})
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//    engine.impl.EngineTest.class
+//})
 
 public class X11DependedTest {}
