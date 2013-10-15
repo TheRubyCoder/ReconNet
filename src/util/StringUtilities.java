a0da7ea7047e171f4f87894ab4b7272c3af69957
Gerhard Oelker <oelker@informatik.haw-hamburg.de>
2013-10-15 10:53:32 +0200

    added example to spec comment

--- src/util/StringUtilities.java
+++ src/util/StringUtilities.java
@@ -1,18 +1,20 @@
 package util;
 
-public class StringUtilities {
+public class StringUtilities{
     /**
-     * Returns the substring before the last occurrence of a delimiter. The
-     * delimiter is not part of the result.
+     * Returns the substring before the last occurrence of a given delimiter -
+     * excluding this delimiter
      *
-     * @param string    String to get a substring from.
+     * @param string    String to process.
      * @param delimiter String to search for.
-     * @return          Substring before the first occurrence of the delimiter.
+     * @return          Substring before the last occurrence of the delimiter.
+     * example:
+     * StringUtilities.substringBeforeLast("Hello.Brave.New.World", ".");
+     * => "Hello.Brave.New"
      */
-    public static String substringBeforeLast( String string, String delimiter )
-    {
-        int pos = string.lastIndexOf( delimiter );
+    public static String substringBeforeLast(String string, String delimiter){
+        int pos = string.lastIndexOf(delimiter);
 
-        return pos >= 0 ? string.substring( 0, pos ) : string;
-    }
+        return pos >= 0 ? string.substring(0, pos) : string;
+    } 
 }
