3366e2960a94cc4043dd543426a233cbdc5d002a
danielroehrborn <daniel.roehrborn@haw-hamburg.de>
2013-11-19 11:16:20 +0100

    nochmal upload

--- src/engine/ihandler/IRulePersistence.java
+++ src/engine/ihandler/IRulePersistence.java
@@ -110,7 +110,7 @@ public interface IRulePersistence {
 	public void setMarking(@NotNull int id, @NotNull Place place, @NotNull int marking) throws EngineException;
 	
 	/**
-	 * Sets the Capacity of a Place.
+	 * Sets the Capacity of a Place
 	 * 
 	 * @param id ID of the Rule
 	 * @param place where to set the Capacity
