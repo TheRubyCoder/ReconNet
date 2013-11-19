4301f707549b9ac8ab2455ce4afe6cb80618f0ea
bjoern <bjoern.kulas@haw-hamburg.de>
2013-11-19 23:12:03 +0100

    -Add Testcase SimulationHandlerTest -Replace incorrect code in class SimulationHandler, to pass the added Testcase

--- src/engine/handler/simulation/SimulationHandler.java
+++ src/engine/handler/simulation/SimulationHandler.java
@@ -47,203 +47,197 @@ import exceptions.ShowAsInfoException;
 
 public class SimulationHandler implements ISimulation {
 
-	/** Session manager holding session data */
-	private final SessionManager sessionManager;
+    /** Session manager holding session data */
+    private final SessionManager sessionManager;
 
-	/** The transformation component (singleton instance) */
-	private final ITransformation transformationComponent;
+    /** The transformation component (singleton instance) */
+    private final ITransformation transformationComponent;
 
-	/** Singleton instance of this class */
-	private static SimulationHandler simulation;
+    /** Singleton instance of this class */
+    private static SimulationHandler simulation;
 
-	/**
-	 * Random number generator to choose a random rule to be applied or to
-	 * choose whether to fire or to transform
-	 */
-	private Random random = new Random();
+    /**
+     * Random number generator to choose a random rule to be applied or to
+     * choose whether to fire or to transform
+     */
+    private Random random = new Random();
 
-	/** The distance for nodes that are added through application of a rule */
-	public static final int DISTANCE_WHEN_ADDED = 100;
+    /** The distance for nodes that are added through application of a rule */
+    public static final int DISTANCE_WHEN_ADDED = 100;
 
-	private SimulationHandler() {
-		sessionManager = SessionManager.getInstance();
-		transformationComponent = TransformationComponent.getTransformation();
+    private SimulationHandler() {
+	sessionManager = SessionManager.getInstance();
+	transformationComponent = TransformationComponent.getTransformation();
+    }
+
+    /** Returns the singleton instance */
+    public static SimulationHandler getInstance() {
+	if (simulation == null)
+	    simulation = new SimulationHandler();
+
+	return simulation;
+    }
+
+    @Override
+    public int createSimulationSession(int id) {
+	return 0;
+    }
+
+    @Override
+    public void fire(int id, int n) throws EngineException {
+
+	PetrinetData petrinetData = sessionManager.getPetrinetData(id);
+
+	// Test: is id valid
+	if (petrinetData == null) {
+	    exception("SimulationHandler - id of the Petrinet is wrong");
+	    return;
 	}
 
-	/** Returns the singleton instance */
-	public static SimulationHandler getInstance() {
-		if (simulation == null)
-			simulation = new SimulationHandler();
+	Petrinet petrinet = petrinetData.getPetrinet();
+	for (int i = 0; i < n; i++) {
 
-		return simulation;
+	    // if there are no more activated transitions the
+	    // petrinet.fire()method will throw a IllegalState Exception
+	    // this method catches the IllegalState Exception and throws a
+	    // EngineException so the GUI only will get EngineExceptions
+	    try {
+		petrinet.fire();
+	    } catch (IllegalStateException e) {
+		exception(e.getMessage());
+	    }
+	}
+    }
+
+    @Override
+    public void save(int id, String path, String filename, String format) throws EngineException {
+	throw new UnsupportedOperationException("Simulation data are not implemented");
+    }
+
+    @Override
+    public void transform(int id, Collection<Integer> ruleIDs, int n) throws EngineException {
+
+	if (ruleIDs.isEmpty()) {
+	    info("Es sind keine Regeln ausgewählt");
 	}
 
-	@Override
-	public int createSimulationSession(int id) {
-		return 0;
+	PetrinetData petrinetData = sessionManager.getPetrinetData(id);
+
+	// Test: is id valid
+	if (petrinetData == null) {
+	    exception("SimulationHandler - id of the Petrinet is wrong");
+	    return;
 	}
 
-	@Override
-	public void fire(int id, int n) throws EngineException {
+	Petrinet petrinet = petrinetData.getPetrinet();
+	JungData jungData = petrinetData.getJungData();
+	List<Rule> sortedRules = new ArrayList<Rule>();
+	for (Integer ruleId : ruleIDs) {
+	    sortedRules.add(sessionManager.getRuleData(ruleId).getRule());
+	}
+	sortedRules = Collections.unmodifiableList(sortedRules);
 
-		PetrinetData petrinetData = sessionManager.getPetrinetData(id);
+	for (int i = 0; i < n; i++) {
+	    // Make sure a random rule is selected each time
+	    List<Rule> shuffledRules = new ArrayList<Rule>(sortedRules);
+	    Collections.shuffle(shuffledRules);
+	    // Find matching rule and apply it
+	    Transformation transformation = findMatchingRule(shuffledRules, petrinet);
+	    if (!(transformation == null)) {// p2 nach präsentation
+		// Remove deleted elements from display
+		jungData.deleteDataOfMissingElements(petrinet);
+		// Add new elements to display
+		fillJungDataWithNewElements(jungData, transformation.getAddedPlaces(), transformation.getAddedTransitions(), transformation.getAddedPreArcs(), transformation.getAddedPostArcs());
+	    } else {// p2 nach präsentation
+		info("Keine der Regeln passt auf das Petrinetz");// p2 nach  präsentation
+	    }// p2 nach präsentation
+	}
+    }
 
-		// Test: is id valid
-		if (petrinetData == null) {
-			exception("SimulationHandler - id of the Petrinet is wrong");
-			return;
-		} 
-		
-		Petrinet petrinet = petrinetData.getPetrinet();
-		for (int i = 0; i < n; i++) {
+    /**
+     * After a rule has been applied on a petrinet, the added nodes are not part
+     * of the display (<code>jungData</code>). They must be added.
+     * 
+     * @param jungData
+     * @param addedNodes
+     * @param addedArcs
+     */
+    private void fillJungDataWithNewElements(JungData jungData, Set<Place> addedPlaces, Set<Transition> addedTransitions, Set<PreArc> addedPreArcs, Set<PostArc> addedPostArcs) {
 
-			// if there are no more activated transitions the
-			// petrinet.fire()method will throw a IllegalState Exception
-			// this method catches the IllegalState Exception and throws a
-			// EngineException so the GUI only will get EngineExceptions
-			try {
-				petrinet.fire();
-			} catch (IllegalStateException e) {
-				exception(e.getMessage());
-			}
-		}
+	// Add places at "random" position
+	for (Place place : addedPlaces) {
+	    jungData.createPlace(place);
 	}
 
-	@Override
-	public void save(int id, String path, String filename, String format)
-			throws EngineException {
-		throw new UnsupportedOperationException(
-				"Simulation data are not implemented");
+	// Add transitions at "random" position
+	for (Transition transition : addedTransitions) {
+	    jungData.createTransition(transition);
 	}
 
-	@Override
-	public void transform(int id, Collection<Integer> ruleIDs, int n)
-			throws EngineException {
-
-		if (ruleIDs.isEmpty()) {
-			info("Es sind keine Regeln ausgewählt");
-		}
-
-		PetrinetData petrinetData = sessionManager.getPetrinetData(id);
-
-		// Test: is id valid
-		if (petrinetData == null) {
-			exception("SimulationHandler - id of the Petrinet is wrong");
-			return;
-		} 
-	
-		Petrinet petrinet = petrinetData.getPetrinet();
-		JungData jungData = petrinetData.getJungData();
-		List<Rule> sortedRules = new ArrayList<Rule>();
-		for (Integer ruleId : ruleIDs) {
-			sortedRules.add(sessionManager.getRuleData(ruleId).getRule());
-		}
-		sortedRules = Collections.unmodifiableList(sortedRules);
-
-		for (int i = 0; i < n; i++) {
-			// Make sure a random rule is selected each time
-			List<Rule> shuffledRules = new ArrayList<Rule>(sortedRules);
-			Collections.shuffle(shuffledRules);
-			// Find matching rule and apply it
-			Transformation transformation = findMatchingRule(shuffledRules,
-					petrinet);
-			// Remove deleted elements from display
-			jungData.deleteDataOfMissingElements(petrinet);
-			// Add new elements to display
-			fillJungDataWithNewElements(
-				jungData,
-				transformation.getAddedPlaces(),
-				transformation.getAddedTransitions(),
-				transformation.getAddedPreArcs(),
-				transformation.getAddedPostArcs()
-			);
-		}
+	// Add arcs
+	for (PreArc arc : addedPreArcs) {
+	    // Find out which method to call
+	    jungData.createArc(arc, arc.getSource(), arc.getTarget());
 	}
 
-	/**
-	 * After a rule has been applied on a petrinet, the added nodes are not part
-	 * of the display (<code>jungData</code>). They must be added.
-	 * 
-	 * @param jungData
-	 * @param addedNodes
-	 * @param addedArcs
-	 */
-	private void fillJungDataWithNewElements(JungData jungData,
-			Set<Place> addedPlaces, Set<Transition> addedTransitions,
-			Set<PreArc> addedPreArcs, Set<PostArc> addedPostArcs) {
-
-		//Add places at "random" position
-		for (Place place : addedPlaces) {
-			jungData.createPlace(place);
-		}
-
-		//Add transitions at "random" position
-		for (Transition transition : addedTransitions) {
-			jungData.createTransition(transition);
-		}
-		
-		//Add arcs
-		for (PreArc arc : addedPreArcs) {
-			//Find out which method to call
-			jungData.createArc(arc, arc.getSource(), arc.getTarget());
-		}
-
-		for (PostArc arc : addedPostArcs) {
-			//Find out which method to call			
-			jungData.createArc(arc, arc.getSource(), arc.getTarget());
-		}
+	for (PostArc arc : addedPostArcs) {
+	    // Find out which method to call
+	    jungData.createArc(arc, arc.getSource(), arc.getTarget());
 	}
+    }
 
-	/**
-	 * Finds a rule out of <code>shuffledRules</code> that can be applied on
-	 * <code>petrinet</code>
-	 * 
-	 * @param shuffledRules
-	 * @param petrinet
-	 * @return Transformation that was used for altering the petrinet
-	 * @throws ShowAsInfoException
-	 *             if none of the rules matches with the petrinet (user friendly
-	 *             message)
-	 */
-	private Transformation findMatchingRule(List<Rule> shuffledRules,
-			Petrinet petrinet) {
-		Iterator<Rule> ruleIterator = shuffledRules.iterator();
-		while (ruleIterator.hasNext()) {
-			Rule rule = (Rule) ruleIterator.next();
-			Transformation transformation = transformationComponent.transform(
-					petrinet, rule);
-			if (transformation == null) {
-				// go on with iteration
-			} else {
-				return transformation;
-			}
-		}
-		info("Keine der Regeln passt auf das Petrinetz");
-		return null;
+    /**
+     * Finds a rule out of <code>shuffledRules</code> that can be applied on
+     * <code>petrinet</code>
+     * 
+     * @param shuffledRules
+     * @param petrinet
+     * @return Transformation that was used for altering the petrinet
+     * @throws ShowAsInfoException
+     *             if none of the rules matches with the petrinet (user friendly
+     *             message)
+     */
+    private Transformation findMatchingRule(List<Rule> shuffledRules, Petrinet petrinet) {
+	Iterator<Rule> ruleIterator = shuffledRules.iterator();
+	while (ruleIterator.hasNext()) {
+	    Rule rule = (Rule) ruleIterator.next();
+	    Transformation transformation = transformationComponent.transform(petrinet, rule);
+	    if (transformation == null) {
+		// go on with iteration
+	    } else {
+		return transformation;
+	    }
 	}
+	// info("Keine der Regeln passt auf das Petrinetz"); p2 nach
+	// präsentation
+	return null;
+    }
 
-	@Override
-	public void fireOrTransform(int id, Collection<Integer> ruleIDs, int n)
-			throws EngineException {
-		for (int i = 0; i < n; i++) {
-			//try fire first? 50 : 50 chance
-			if (random.nextFloat() < 0.5d) {
-				try {
-					fire(id, 1);
-				} catch (EngineException ex) {
-					//fire failed? OK, try transform
-					transform(id, ruleIDs, 1);
-				}
-			} else {
-				//try transform first? 50 : 50 chance
-				try {
-					transform(id, ruleIDs, 1);
-				} catch (EngineException ex) {
-					// transform failed? OK, try fire
-					fire(id, 1);
-				}
-			}
+    @Override
+    public void fireOrTransform(int id, Collection<Integer> ruleIDs, int n) throws EngineException {
+	for (int i = 0; i < n; i++) {
+	    // try fire first? 50 : 50 chance
+	    if (random.nextFloat() < 0.5d) {
+		try {
+		    fire(id, 1);
+		} catch (EngineException ex) {
+		    // fire failed? OK, try transform
+		    transform(id, ruleIDs, 1);
 		}
+	    } else {
+		// try transform first? 50 : 50 chance
+		try {
+		    transform(id, ruleIDs, 1);
+		} catch (EngineException ex) {
+		    // transform failed? OK, try fire
+		    fire(id, 1);
+		}
+		catch(ShowAsInfoException ex){
+		 // transform failed? OK, try fire
+		    fire(id, 1);
+		}
+	    }
 	}
+    }
 
 }
---
+++ test/engine/handler/SimulationHandlerTest.java
@@ -0,0 +1,105 @@
+package engine.handler;
+
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.junit.Test;
+import petrinet.model.Place;
+import petrinet.model.Transition;
+import transformation.ITransformation;
+import transformation.Rule;
+import transformation.TransformationComponent;
+import engine.data.RuleData;
+import engine.handler.petrinet.PetrinetPersistence;
+import engine.handler.simulation.SimulationHandler;
+import engine.ihandler.IPetrinetPersistence;
+import engine.ihandler.ISimulation;
+import engine.session.SessionManager;
+import exceptions.EngineException;
+
+public class SimulationHandlerTest {
+
+	@Test
+	public void fireOrTransformTest() {
+		try {
+
+			IPetrinetPersistence iPetrinetPersistence = PetrinetPersistence
+					.getInstance();
+			// petrinetz erstellen
+			int petrinetID = iPetrinetPersistence.createPetrinet();
+			// Koordinaten für alle Stellen festlegen
+			Point2D place1Point = new Point2D.Double(50., 50.);
+			Point2D place2Point = new Point2D.Double(400., 50.);
+			Point2D place4Point = new Point2D.Double(50., 200.);
+			Point2D place3Point = new Point2D.Double(400., 200.);
+			// Stellen erstellen und in Petrinet einfügen
+			Place place1 = iPetrinetPersistence.createPlace(petrinetID,
+					place1Point);
+			Place place2 = iPetrinetPersistence.createPlace(petrinetID,
+					place2Point);
+			Place place3 = iPetrinetPersistence.createPlace(petrinetID,
+					place3Point);
+			Place place4 = iPetrinetPersistence.createPlace(petrinetID,
+					place4Point);
+			// Label ändern
+			iPetrinetPersistence.setPname(petrinetID, place3, "defined");
+			// Markierung einfügen
+			iPetrinetPersistence.setMarking(petrinetID, place1, 1);
+
+			// Transitionen erstellen
+			// Koordinaten für alle Transitionen festlegen
+			Point2D transition1Point = new Point2D.Double(200., 50.);
+			Point2D transition4Point = new Point2D.Double(50., 100.);
+			Point2D transition2Point = new Point2D.Double(400., 100.);
+			Point2D transition3Point = new Point2D.Double(200., 200.);
+			// Transitionen erstellen und in Petrinet einfügen
+			Transition trans1 = iPetrinetPersistence.createTransition(
+					petrinetID, transition1Point);
+			Transition trans2 = iPetrinetPersistence.createTransition(
+					petrinetID, transition2Point);
+			Transition trans3 = iPetrinetPersistence.createTransition(
+					petrinetID, transition3Point);
+			Transition trans4 = iPetrinetPersistence.createTransition(
+					petrinetID, transition4Point);
+
+			// Kanten erstellen und in Petrinet einfügen
+			// PRE's
+			iPetrinetPersistence.createPreArc(petrinetID, place1, trans1);
+			iPetrinetPersistence.createPreArc(petrinetID, place2, trans2);
+			iPetrinetPersistence.createPreArc(petrinetID, place3, trans3);
+			iPetrinetPersistence.createPreArc(petrinetID, place4, trans4);
+			// Post's
+			iPetrinetPersistence.createPostArc(petrinetID, trans4, place1);
+			iPetrinetPersistence.createPostArc(petrinetID, trans3, place4);
+			iPetrinetPersistence.createPostArc(petrinetID, trans2, place3);
+			iPetrinetPersistence.createPostArc(petrinetID, trans1, place2);
+
+			ITransformation iTransformation = TransformationComponent.getTransformation();
+			Rule rule = iTransformation.createRule();
+			RuleData ruleData = SessionManager.getInstance().createRuleData(rule);
+			int ruleId = ruleData.getId();
+			iTransformation.storeSessionId(ruleId, rule);
+			Place placeK = rule.addPlaceToK("defined");
+			Place placeL = rule.fromKtoL(placeK);
+			Place placeR = rule.fromKtoR(placeK);
+			rule.setMarkInK(placeL, 1);
+			Transition transRule = rule.addTransitionToR(""); 
+			rule.addPostArcToR("", transRule, placeR);
+			rule.addPreArcToR("", placeR, transRule);
+			
+			// Simulation
+			ISimulation iSimulation = SimulationHandler.getInstance();
+			Collection<Integer> ruleIDs = new ArrayList<Integer>();
+			ruleIDs.add(ruleId);
+
+			iSimulation.fireOrTransform(petrinetID, ruleIDs, 2000);
+
+		} catch (EngineException e) {
+			e.printStackTrace();
+		}
+		assert (true);
+	}
+
+
+}
