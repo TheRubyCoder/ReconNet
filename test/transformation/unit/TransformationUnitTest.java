4124658a12556c7904627a7b7508acd37f78a32e
Christian Hoff <hoff.christian@gmx.de>
2016-08-24 02:25:00 +0200

    some test cases

--- test/transformation/unit/TransformationUnitTest.java
+++ test/transformation/unit/TransformationUnitTest.java
@@ -53,6 +53,7 @@ package transformation.unit;
 
 import java.awt.geom.Point2D;
 import java.util.HashMap;
+import java.util.UUID;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -65,6 +66,7 @@ import engine.ihandler.IPetrinetManipulation;
 import engine.ihandler.IRuleManipulation;
 import engine.ihandler.IRulePersistence;
 import engine.ihandler.ITransformationUnitManipulation;
+import engine.session.SessionManager;
 import exceptions.EngineException;
 import gui.EngineAdapter;
 
@@ -113,6 +115,10 @@ public class TransformationUnitTest {
     ruleNameToIdMapping.put("r4", this.r4);
   }
 
+  // Executes two rules:
+  // r1 adds place p1
+  // r2 removes place p1
+  // r1;r2 is a legit sequence
   @Test
   public void testSequenceOperator()
     throws EngineException {
@@ -129,10 +135,6 @@ public class TransformationUnitTest {
         new Point2D.Double(0, 0));
     this.ruleManipulation.setPname(this.r2, p1b, "p1");
 
-    // Sequence R1;R2 must be possible
-    // R1 adds place p1
-    // R2 removes place p1
-
     // File location can be ignored
     int transformationUnitId =
       this.transformationUnitManipulation.createTransformationUnit("", "");
@@ -146,6 +148,152 @@ public class TransformationUnitTest {
     Assert.assertEquals(
       0,
       this.petrinetManipulation.getJungLayout(petrinetId).getGraph().getVertexCount());
+  }
+
+  // Executes a rules as long as possible
+  // r1 adds a place 'x'
+  // r1 has NAC with 10x place 'x'
+  // r1 can be executed 10 times
+  @Test
+  public void testAsLongAsPossible()
+    throws EngineException {
+
+    UUID nacId = ruleManipulation.createNac(r1);
+
+    Place p = null;
+
+    p =
+      this.rulePersistence.createPlace(this.r1, RuleNet.R,
+        new Point2D.Double(0, 0));
+    this.ruleManipulation.setPname(this.r1, p, "x");
+
+    for (int i = 0; i < 10; i++) {
+      p =
+        this.rulePersistence.createPlace(this.r1, nacId, new Point2D.Double(
+          0, i * 50));
+      this.ruleManipulation.setPname(this.r1, nacId, p, "x");
+    }
+
+    int transformationUnitId =
+      this.transformationUnitManipulation.createTransformationUnit("", "");
+
+    this.transformationUnitManipulation.setControlExpression(
+      transformationUnitId, "r1!");
+
+    this.transformationUnitManipulation.executeTransformationUnit(
+      transformationUnitId, petrinetId, ruleNameToIdMapping);
+
+    Assert.assertEquals(10, this.petrinetManipulation.getJungLayout(
+      petrinetId).getGraph().getVertexCount());
+  }
+
+  // Executes a rules a random number of times
+  // r1 adds a place 'x'
+  // r1 has NAC with 10x place 'x'
+  // r1 can be executed 0..10 times
+  @Test
+  public void testRandomNumberOfTimes()
+    throws EngineException {
+
+    UUID nacId = ruleManipulation.createNac(r1);
+
+    Place p = null;
+
+    p =
+      this.rulePersistence.createPlace(this.r1, RuleNet.R,
+        new Point2D.Double(0, 0));
+    this.ruleManipulation.setPname(this.r1, p, "x");
+
+    for (int i = 0; i < 10; i++) {
+      p =
+        this.rulePersistence.createPlace(this.r1, nacId, new Point2D.Double(
+          0, i * 50));
+      this.ruleManipulation.setPname(this.r1, nacId, p, "x");
+    }
+
+    int transformationUnitId =
+      this.transformationUnitManipulation.createTransformationUnit("", "");
+
+    this.transformationUnitManipulation.setControlExpression(
+      transformationUnitId, "r1*");
+
+    this.transformationUnitManipulation.executeTransformationUnit(
+      transformationUnitId, petrinetId, ruleNameToIdMapping);
+
+    int cntPlaces =
+      this.petrinetManipulation.getJungLayout(petrinetId).getGraph().getVertexCount();
+
+    Assert.assertTrue((cntPlaces >= 0) && (cntPlaces <= 10));
+  }
+
+  // Bad test design - is only legit after subsequent test runs
+  // Executes one of two rules
+  // r1: adds place 'x'
+  // r2: adds place 'y'
+  // checks afterwards, if either a place 'x' or 'y' is present
+  @Test
+  public void testAlternative()
+    throws EngineException {
+
+    Place x =
+      this.rulePersistence.createPlace(this.r1, RuleNet.R,
+        new Point2D.Double(0, 0));
+    this.ruleManipulation.setPname(this.r1, x, "x");
+
+    Place y =
+      this.rulePersistence.createPlace(this.r2, RuleNet.R,
+        new Point2D.Double(0, 0));
+    this.ruleManipulation.setPname(this.r2, y, "y");
+
+    int transformationUnitId =
+      this.transformationUnitManipulation.createTransformationUnit("", "");
+
+    this.transformationUnitManipulation.setControlExpression(
+      transformationUnitId, "r1|r2");
+
+    this.transformationUnitManipulation.executeTransformationUnit(
+      transformationUnitId, petrinetId, ruleNameToIdMapping);
+
+    for (Place p : SessionManager.getInstance().getPetrinetData(petrinetId).getPetrinet().getPlaces()) {
+
+      Assert.assertTrue((p.getName().equals("x"))
+        || (p.getName().equals("y")));
+    }
+  }
+
+  // Tests the recovery of the petrinet after execution of the transformation
+  // unit failed
+  // Executes r1 several times and then r2, which will fail
+  // r1 adds place 'x'
+  // r2 removes place 'y' which won't be present
+  // petrinet must be recovered to initial state
+  @Test
+  public void testRecoveryAfterFail()
+    throws EngineException {
+
+    Place x =
+      this.rulePersistence.createPlace(this.r1, RuleNet.R,
+        new Point2D.Double(0, 0));
+    this.ruleManipulation.setPname(this.r1, x, "x");
+
+    Place y =
+      this.rulePersistence.createPlace(this.r2, RuleNet.L,
+        new Point2D.Double(0, 0));
+    this.ruleManipulation.setPname(this.r2, y, "y");
+
+    int transformationUnitId =
+      this.transformationUnitManipulation.createTransformationUnit("", "");
+
+    this.transformationUnitManipulation.setControlExpression(
+      transformationUnitId, "r1;r1;r1;r1;r2");
+
+    try {
+      this.transformationUnitManipulation.executeTransformationUnit(
+        transformationUnitId, petrinetId, ruleNameToIdMapping);
+    } catch (EngineException e) {
+      Assert.assertEquals(0, SessionManager.getInstance().getPetrinetData(
+        petrinetId).getPetrinet().getPlaces().size());
+    }
   }
 
 }
